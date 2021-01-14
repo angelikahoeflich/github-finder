7700ba5e7212282cbdf3b879ff5636de92a94ee5
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2021-01-14 11:36:37 -0800

    displaying personal users data

--- src/Components/Users/User.js
+++ src/Components/Users/User.js
@@ -24,6 +24,7 @@ class User extends Component {
       blog,
       login,
       html_url,
+      company,
       followers,
       following,
       public_repos,
@@ -41,6 +42,52 @@ class User extends Component {
         </Link>
         Hireable : { ' '}
         {hireable ? <i className="fas fa-check text-success"/> : <i className="fas fa-times-circle text-danger"/>}
+        <div className="card grid-2">
+          <div className="all-center">
+            <img src={avatar_url} className="round-img" style={{width: '130px'}}alt="profile"/>
+            <h1>{name}</h1>
+            <p>Location:{location}</p>
+          </div>
+          <div>
+            {bio && <Fragment>
+              <h3>Bio:</h3>
+              <p>{bio}</p>
+              </Fragment>}
+              <a href={html_url} className="btn btn-dark my-1">Visit Github Profile</a>
+              <ul>
+                <li>
+                  {login && <Fragment>
+                    <strong>Username:</strong> {login}
+                    </Fragment>}
+                </li>
+                <li>
+                  {company && <Fragment>
+                    <strong>Company:</strong> {company}
+                    </Fragment>}
+                </li>
+                <li>
+                  {blog && <Fragment>
+                    <strong>Website:</strong> {blog}
+                    </Fragment>}
+                </li>
+              </ul>
+          </div>
+        </div>
+
+        <div className="card text-center">
+          <div className="badge badge-primary">
+            Followers: {followers}
+          </div>
+          <div className="badge badge-success">
+            Following: {following}
+          </div>
+          <div className="badge badge-light">
+            Public Repos: {public_repos}
+          </div>
+          <div className="badge badge-dark">
+            Public Gists: {public_gists}
+          </div>
+        </div>
       </Fragment>
     )
   }
