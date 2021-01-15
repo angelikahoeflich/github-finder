b4c06e0150210a85c647056de91a97d8ba0eaa9f
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2021-01-15 14:23:39 -0800

    home page and not found

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
-import React, { Fragment} from 'react';
+import React from 'react';
 import Navbar from './Components/Layout/Navbar';
-import Users from './Components/Users/Users';
 import User from './Components/Users/User';
-import Search from './Components/Users/Search';
 import Alert from './Components/Layout/Alert';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import About from './Components/Pages/About';
 import GitHubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
+import Home from './Components/Pages/Home'
+import NotFound from './Components/Pages/NotFound';
 
 
 const App = () =>  {
@@ -22,17 +22,12 @@ const App = () =>  {
         <div className="container"> 
           <Alert />
           <Switch>
-            <Route exact path='/' render={props => (
-              <Fragment>
-                  <Search 
-          />
-          <Users />
-              </Fragment>
-            )}/>
+            <Route exact path='/' component={Home}/>
 
           <Route exact path='/about' component={About}/>
 
           <Route exact path='/user/:login' component={User} />
+          <Route component={NotFound}/>
           </Switch>
         
         </div>
